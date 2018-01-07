b99bc99d112e75e5850f516e0a3f62335c902847
Tom Friederich <tfriederich@live.nl>
2018-01-07 13:23:55 +0100

    Add menu items

--- app/src/main/java/f/tom/consistentspacingdemo/MainActivity.java
+++ app/src/main/java/f/tom/consistentspacingdemo/MainActivity.java
@@ -2,6 +2,8 @@ package f.tom.consistentspacingdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
